f340ff6120368bcaac6ac9e934050e5fc140e46e
Ephratah63 <ephratah.girma@aastustudent.edu.et>
2025-12-25 05:44:41 +0300

    Cleanup: reorganize source files, remove clutter

--- WedajMoodCompanion/src/com/wedaj/core/Mood.java
+++
@@ -1,5 +0,0 @@
-package com.wedaj.core;
-
-public enum Mood {
-    HAPPY, SAD, ANGRY, NEUTRAL
-}
--- WedajMoodCompanion/src/com/wedaj/core/SadResponse.java
+++
@@ -1,8 +0,0 @@
-package com.wedaj.core;
-
-public class SadResponse extends MoodResponse {
-    @Override
-    public String reply() {
-        return "Sad response"; // placeholder
-    }
-}
